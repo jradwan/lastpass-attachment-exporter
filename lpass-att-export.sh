f18ea2b528659d5538ee561e3cfdae4da33be330
Jeremy C. Radwan <jradwan@users.noreply.github.com>
2021-03-04 22:14:34 -0500

    add TODOs

--- lpass-att-export.sh
+++ lpass-att-export.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# TODO
+# - suppress errors on shared folders
+# - command-line parameters:
+#   - verbose: full logging
+#   - export : actually export attachments (default is test-only mode for list)
+
 mkdir -p lpass-export
 cd lpass-export
 
 for id in `lpass ls | sed -n "s/^.*id:\s*\([0-9]*\).*$/\1/p"`; do
 
+  # TODO: enable for verbose mode
+  #echo "Checking: " ${id}
+
+  # TODO: 'Error: Could not find specified account(s).' messages are caused by shared folders, suppress them?
   path=`lpass show ${id} | sed "1q;d" | awk '{$NF=""; print $0}' | awk '{$NF=""; print $0}' | awk '{$1=$1};1'`
   attcount=`lpass show ${id} | grep att- | wc -l`
 
@@ -25,9 +35,12 @@ for id in `lpass ls | sed -n "s/^.*id:\s*\([0-9]*\).*$/\1/p"`; do
         out=${path}/${attcount}_${attname}
     fi
 
-    echo ${id} - ${path} ": " ${attid} "-" ${attname} " > " ${out}
+    # TODO: enable for verbose mode
+    #echo ${id} - ${path} ": " ${attid} "-" ${attname} " > " ${out}
+    echo ${out}
 
-    lpass show --attach=${attid} ${id} --quiet > "${out}"
+    # TODO: enable for export mode
+    #lpass show --attach=${attid} ${id} --quiet > "${out}"
 
     let attcount-=1
   done
